0c469d752f375767b69a96c48e6d7c7cbbacb5c7
JeliHacker <jeligooch@gmail.com>
2019-11-06 18:17:10 -0500

    a bunch o stuff

--- default.js
+++ default.js
@@ -5,12 +5,12 @@ penguinPromise.then
     function(data)
     {
         console.log("nice code broski");
-        console.log(data);
+        console.log("data", data);
         console.log(getQuizGrade(data, 0));
     },
     function(err)
     {
-        console.log("this code is very not good", err);
+        console.log("this code is not good", err);
     }
 )
 
@@ -22,9 +22,10 @@ var getQuiz = function(penguin)
 
 var getQuizGrade = function(penguins, index)//index is day
 {
-    console.log(penguins)
+    console.log("penguins", penguins)
     console.log("penguin.quizes", penguins[0].quizes)
     
+    //an attempt to plot points
     d3.select("svg")
     .selectAll("circle")
     .data(penguins)
@@ -33,17 +34,46 @@ var getQuizGrade = function(penguins, index)//index is day
     .text(function(penguin){
         return penguin.quizes[index].grade
     })
+    .attr("r", 20)
+    .attr("cy", function(penguin)
+        {
+            for (i = 0; i < 230; i + 10){
+                return i + 20;
+            }
+        })
+    
+    //puts out the numbers
+    d3.select("body")
+    .selectAll("p")
+    .data(penguins)
+    .enter()
+    .append("p")
+    .text(function(penguin){
+        return penguin.quizes[index].grade
+    })
     .attr("r", 2)
     
-    return penguins[0].quizes[0].grade;
     
+    //make buttons for each day WORK IN PROGRESS
+    d3.select("body")
+    .selectAll("input")
+    .data(penguins)
+    .enter()
+    .append("button")
+    .attr("type","text")
+    .text("day")
+    .attr("id", function(penguin){return penguin.quizes.day})
+    
+    
+    //so far, this is just for information purposes
+    return penguins[0].quizes[0].grade;
 }
 
 
 
 
 
-var yscale = d3.scaleLiner()
+//var yscale = d3.scaleLiner()
 
 var xData = d3.range(10);
 
@@ -51,8 +81,34 @@ var points = xData.map(function(x)
                       {
     return {
         x:x,
-        y:getQuizGrade(penguin, x)
     };
 })
+
+console.log("works up to this point")
+
+var setup = function(points)
+{
+    d3.select("svg")
+    .attr("width", screen.width)
+}
+
+var drawGraph = function(points)
+{
+    d3.select("svg")
+    .selectAll("circle")
+    .data(points)
+    .enter()
+    .append("circle")
+    .attr("x", function(point) {return point.x})
+    .attr("y", function(point){return point.y})
+    
+}
+
+
+
+
+
+
+
 
 //var points =    //points is the array of all grades on a given day
